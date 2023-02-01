84120c398d5f85ba2411b4ba88bdc3771491d7c6
John Doe <git@ton.cash>
2023-02-01 21:08:14 +0100

    linted

--- app.py
+++ app.py
@@ -1,38 +1,43 @@
+# pylint: disable = no-name-in-module
+"""Telegram bot for OpenAI GPT-3 chatbot"""
+from datetime import datetime
 from revChatGPT.Official import Chatbot
 from revChatGPT.Official import Prompt
 from decouple import config
-from datetime import datetime
-import telebot
-from conv import load, appendQuestion, appendResponse, get, init, reset, rollback
 from transliterate import translit
+import telebot
+from conv import load, get, init, reset, rollback
+from conv import save_question, save_response
 
 # Get API key from .env file
 API_KEY = config("OPENAI_TOKEN")
 BOT_TOKEN= config("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
 chatbots = {}
-conversation_history = {}
 BOT_NAME = bot.get_me().username
 print(f"Loaded, bot name: @{BOT_NAME}")
 
 @bot.message_handler(commands=['reset'])
-def reset(message):
+def reset_event(message):
+    """Reset chat history"""
     if message.chat.id not in chatbots:
         bot.reply_to(message, "Chat history is empty")
         return
-    reset(message.chat.id);
+    reset(message.chat.id)
     bot.reply_to(message, "Chat history reset")
 
 @bot.message_handler(commands=['rollback'])
-def rollback(message):
+def rollback_event(message):
+    """Rollback one message"""
     if message.chat.id not in chatbots:
         bot.reply_to(message, "Chat history is empty")
         return
-    rollback(message.chat.id);
+    rollback(message.chat.id, 1)
     bot.reply_to(message, "Chat history rollback")
 
 @bot.message_handler(commands=['help'])
-def help(message):
+def help_message(message):
+    """Display help message"""
     bot.reply_to(message, """
     /help - Display this message
     /rollback - Rollback chat history
@@ -41,38 +46,40 @@ def help(message):
     
 @bot.message_handler(func=lambda message: True)
 def reply(message):
-    if(message.chat.id not in chatbots):
-      chatbots[message.chat.id] = Chatbot(api_key=API_KEY)
-      load(message.chat.id);
-      init(message.chat.id, message.chat.title, message.chat.type, message.from_user);
-      # Inject prompt
-      prompt = Prompt()
-      prompt.chat_history = get(message.chat.id)
-      for i in range(len(prompt.chat_history)):
-          print(f"Prompt {i}: {prompt.chat_history[i]}")
-      chatbots[message.chat.id].prompt = prompt
-      print(f"Created chatbot for chat {message.chat.id}")
+    """Handle all messages"""
+    if message.chat.id not in chatbots:
+        chatbots[message.chat.id] = Chatbot(api_key=API_KEY)
+        load(message.chat.id)
+        init(message.chat.id, message.chat.title, message.chat.type, message.from_user)
+        # Inject prompt to chatbot
+        prompt = Prompt()
+        prompt.chat_history = get(message.chat.id)
+        chatbots[message.chat.id].prompt = prompt
+        print(f"[BOT] Created chatbot for chat {message.chat.id}")
 
     # If message not from private chat, it must start with bot name
     if message.chat.type != "private" and not message.text.startswith(f"@{BOT_NAME}"):
-       return;
+        return
     else:
-       message.text = message.text.replace(f"@{BOT_NAME}", "").strip()
+        message.text = message.text.replace(f"@{BOT_NAME}", "").strip()
 
     # Allow only latin characters
     if not message.text.isascii():
         message.text = translit(message.text, "ru", reversed=True)
         # Non latin characters are very slow to process and badly tokenized
+    # Save message to chat history
+    save_question(message.chat.id, message.text, message.from_user)
 
-    appendQuestion(message.chat.id, message.text, message.from_user);
+    # Send typing status
+    bot.send_chat_action(message.chat.id, 'typing')
 
-    chatbot = chatbots[message.chat.id]
-    response = chatbot.ask(message.text)
-    currentDateTime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    currentContext = message.chat.id;
-    print(f"[{currentDateTime}] ({message.from_user.first_name} in {currentContext}) {message.text} ~{message.chat.type}")
-    bot.reply_to(message, response["choices"][0]["text"])
-    appendResponse(message.chat.id, response["choices"][0]["text"]);
+    try:
+        resp = chatbots[message.chat.id].ask(message.text)
+        time = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+        print(f"[{time}] ({message.from_user.id} in {message.chat.id}) {message.text}")
+        bot.reply_to(message, resp["choices"][0]["text"])
+        save_response(message.chat.id, resp["choices"][0]["text"])
+    except Exception as ex:
+        print(f"Error: {ex}")
 
 bot.infinity_polling()
-
--- conv.py
+++ conv.py
@@ -1,151 +1,163 @@
-from transliterate import translit
+"""Conversation history module"""
 import os
 import json
+from transliterate import translit
 logDir = os.path.join(os.getcwd(), "logs")
 conversation_history = {}
 
-def userToStr(user, withId=True):
-    if user == None:
-        return "None";
-    
-    first_name = ruToEn(user['first_name'] if 'first_name' in user else "");
-    last_name = ruToEn(user['last_name'] if 'last_name' in user else "");
-    username = user['username'] if 'username' in user else "";
-    if withId:
-        return f'#{user["id"]} is {first_name} {last_name} (@{username})';
+def user_to_str(user, with_id=True):
+    """Convert user object to string"""
+    if user is None:
+        return "None"
+    first_name = trans(user['first_name'] if 'first_name' in user else "")
+    last_name = trans(user['last_name'] if 'last_name' in user else "")
+    username = user['username'] if 'username' in user else ""
+    if with_id:
+        return f'#{user["id"]} is {first_name} {last_name} (@{username})'
     else:
-        return f'{first_name} {last_name} (aka @{username})';
+        return f'{first_name} {last_name} (aka @{username})'
 
-def ruToEn(text):
-    return translit(text, 'ru', reversed=True);
+def trans(text):
+    """Translate text to english"""
+    return translit(text, 'ru', reversed=True)
 
-def get(chatId):
-    strId = str(chatId);
-    if not strId in conversation_history:
-        return [];
+def get(chat_id):
+    """Get conversation history for Prompt"""
+    str_id = str(chat_id)
+    if str_id not in conversation_history:
+        return []
     # Generate prompt from history. Using title, type and member list
-    prompt = f'You are chat bot. Chat title: "{conversation_history[strId]["title"]}"\n';
+    prompt = f'You are chat bot. Chat title: "{conversation_history[str_id]["title"]}"\n'
 
-    if conversation_history[strId]['type'] == "private":
-        memberObj = conversation_history[strId]['members'];
+    if conversation_history[str_id]['type'] == "private":
+        member_obj = conversation_history[str_id]['members']
         # Get first member
-        member = memberObj[list(memberObj.keys())[0]];
-        member = userToStr(member, False);
-        prompt += f'Your companion is {member}\n';
+        member = member_obj[list(member_obj.keys())[0]]
+        member = user_to_str(member, False)
+        prompt += f'Your companion is {member}\n'
     else:
-        prompt += f'You are in group chat with {len(conversation_history[strId]["members"])} members.\n';
+        members_count = len(conversation_history[str_id]["members"])
+        prompt += f'You are in group chat with {members_count} members.\n'
+        # TODO: Add members list to prompt
 
-    realHistory = conversation_history[strId]['history'];
+    real_history = conversation_history[str_id]['history']
     # Concat prompt with history in array [prompt, ...history]
     # if one line is match one of prompt variants, it will be ignored
-    ignoreLines = ['You are chat bot', 'Your companion', 'You are in group chat']
+    ignore_lines = ['You are chat bot', 'Your companion', 'You are in group chat']
     # Filter history
-    realHistory = [line for line in realHistory if not any(ignore in line for ignore in ignoreLines)]
+    real_history = [
+        line for
+        line in real_history
+        if not any(ignore in line for ignore in ignore_lines)
+    ]
     # Add prompt to beginning of history
-    realHistory = [prompt] + realHistory;
-    return realHistory;
+    real_history = [prompt] + real_history
+    return real_history
 
-def load(chatId):
-    strId = str(chatId);
+def load(chat_id):
+    """Load conversation history from file"""
+    str_id = str(chat_id)
     if not os.path.exists(
         os.path.join(
             logDir,
-            strId + ".json",
+            str_id + ".json",
         )
     ):
-        return print (f"Conversation history for chat {strId} not found")
+        return print (f"[ERROR] Conversation history for chat {str_id} not found")
     with open(
-        os.path.join(logDir, strId + ".json",
+        os.path.join(logDir, str_id + ".json",
         ),
         "r",
         encoding="utf-8",
     ) as f:
-        conversation_history[strId] = json.load(f)
-        print(f"Loaded conversation history for chat {strId}")
+        conversation_history[str_id] = json.load(f)
+        print(f"[CONV] Loaded conversation history for chat {str_id}")
 
-def init(chatId, title, type, fromUser):
-    strId = str(chatId);
-    if strId in conversation_history:
-        return; # Already initialized
-
-    if title == None:
+def init(chat_id, title, chat_type, from_user):
+    """Initialize conversation history"""
+    str_id = str(chat_id)
+    if str_id in conversation_history:
+        return # Already initialized
+    if title is None:
         title = 'Private chat'
 
-    conversation_history[strId] = {
-        "id": strId,
-        "title": ruToEn(title),
+    conversation_history[str_id] = {
+        "id": str_id,
+        "title": trans(title),
         "history": [],
         "members": {},
-        "type": type,
-    };
+        "type": chat_type,
+    }
 
-    conversation_history[strId]['members'][fromUser.id] = {
-        "id": fromUser.id,
-        "first_name": ruToEn(fromUser.first_name if fromUser.first_name != None else ""),
-        "last_name": ruToEn(fromUser.last_name if fromUser.last_name != None else ""),
-        "username": fromUser.username,
-    };
+    conversation_history[str_id]['members'][from_user.id] = {
+        "id": from_user.id,
+        "first_name": trans(from_user.first_name if from_user.first_name is not None else ""),
+        "last_name": trans(from_user.last_name if from_user.last_name is not None else ""),
+        "username": from_user.username,
+    }
+    save(str_id)
 
-
-    save(strId);
-
-def save(chatId):
-    strId = str(chatId);
-    if not strId in conversation_history:
-        return;
+def save(chat_id):
+    """Save conversation history to file"""
+    str_id = str(chat_id)
+    if str_id not in conversation_history:
+        return
     with open(
         os.path.join(
             logDir,
-            strId + ".json",
+            str_id + ".json",
         ),
         "w",
         encoding="utf-8",
     ) as f:
-        json.dump(conversation_history[strId], f, indent=4, ensure_ascii=False)
-        print(f"Saved conversation history for chat {strId}")
+        json.dump(conversation_history[str_id], f, indent=4, ensure_ascii=False)
+        print(f"[CONV] Saved conversation history for chat {str_id}")
 
 
-def appendQuestion(chatId, question, author):
-    strId = str(chatId);
-    if not strId in conversation_history:
-        print(f"Error: chat {strId} is not initialized");
-        return;
+def save_question(chat_id, text, author):
+    """Append question to history"""
+    str_id = str(chat_id)
+    if str_id not in conversation_history:
+        print(f"[ERROR] chat {str_id} is not initialized")
+        return
 
-    
     # If member is not in members list, add him
-    if not str(author.id) in conversation_history[strId]['members']:
-        print(f"Adding new member {author.id} to chat {strId}")
-        conversation_history[strId]['members'][str(author.id)] = {
+    if not str(author.id) in conversation_history[str_id]['members']:
+        print(f"Adding new member {author.id} to chat {str_id}")
+        conversation_history[str_id]['members'][str(author.id)] = {
             "id": author.id,
-            "first_name": ruToEn(author.first_name if author.first_name != None else ""),
-            "last_name": ruToEn(author.last_name if author.last_name != None else ""),
+            "first_name": trans(author.first_name if author.first_name is not None else ""),
+            "last_name": trans(author.last_name if author.last_name is not None else ""),
             "username": author.username,
         }
 
-    if(conversation_history[strId]['type'] == 'private'):
-        conversation_history[strId]['history'].append(f'User: {question}');
+    if conversation_history[str_id]['type'] == 'private':
+        conversation_history[str_id]['history'].append(f'User: {text}')
     else:
-        conversation_history[strId]['history'].append(f'#{author.id}: {question}');
-    save(strId);
+        conversation_history[str_id]['history'].append(f'#{author.id}: {text}')
+    save(str_id)
 
-def appendResponse(chatId, response):
-    strId = str(chatId);
-    if not strId in conversation_history:
-        print(f"Error: chat {strId} is not initialized");
-        return;
-    conversation_history[strId]['history'].append(f'Bot: {response}');
-    save(strId);
+def save_response(chat_id, text):
+    """Append response to history"""
+    str_id = str(chat_id)
+    if str_id not in conversation_history:
+        print(f"[ERROR] chat {str_id} is not initialized")
+        return
+    conversation_history[str_id]['history'].append(f'Bot: {text}')
+    save(str_id)
 
-def reset(chatId):
-    strId = str(chatId);
-    if not strId in conversation_history:
-        return;
-    conversation_history[strId]['history'] = [];
-    save(strId);
+def reset(chat_id):
+    """Reset history"""
+    str_id = str(chat_id)
+    if str_id not in conversation_history:
+        return
+    conversation_history[str_id]['history'] = []
+    save(str_id)
 
-def rollback(chatId, count):
-    strId = str(chatId);
-    if not strId in conversation_history:
-        return;
-    conversation_history[strId]['history'] = conversation_history[strId]['history'][:-count];
-    save(strId);
+def rollback(chat_id, count):
+    """Rollback history"""
+    str_id = str(chat_id)
+    if str_id not in conversation_history:
+        return
+    conversation_history[str_id]['history'] = conversation_history[str_id]['history'][:-count]
+    save(str_id)
